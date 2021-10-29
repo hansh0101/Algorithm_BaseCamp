3490ed26d8b1f6c10f93b03cdc8967ae296425b7
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-10-29 23:59:59 +0900

    [9663_N-Queen]

---
+++ soptac_week3/5585.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int n, N, ans;
+int arr[6] = {500, 100, 50, 10, 5, 1};
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin>>n;
+    int idx = 0;
+    N = 1000-n;
+    while(N!=0) {
+        if(N >= arr[idx]) {
+            N-=arr[idx];
+            ans++;
+        }
+        else {
+            idx++;
+        }
+    }
+    cout<<ans;
+    return 0;
+}

47d5fcecfc3da4c4bf1a1243d55df486b578c141
hansh0101 <hansh0101@naver.com>
2022-09-25 19:24:33 +0900

    solved !

---
+++ baekjoon/2743.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string str;
+    cin >> str;
+    cout << str.length() << "\n";
+
+    return 0;
+}

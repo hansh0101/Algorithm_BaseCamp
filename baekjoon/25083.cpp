81dccb57163a85958e2403c17950cc3a75a099d9
hansh0101 <hansh0101@naver.com>
2022-10-25 13:59:44 +0900

    solved !

---
+++ baekjoon/25083.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |";
+
+    return 0;
+}

0f0723d59f786c7a9fd69eabac6c37aa5b6b5745
hansh0101 <hansh0101@inha.edu>
2022-12-06 23:41:04 +0900

    solved !

--- inha_problem_solving_2022_2/w07_a.cpp
+++ inha_problem_solving_2022_2/w07_a.cpp
@@ -1,59 +1,48 @@
 #include <iostream>
-#include <stack>
 #include <cstring>
+#include <stack>
+
 using namespace std;
 
-int T;
+int t;
 string str;
 stack<int> st;
 
-int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	cin >> T;
-	while (T--) {
-		cin >> str;
-		for (char c : str) {
-			if (c == '+') {
-				int num1 = st.top();
-				st.pop();
-				int num2 = st.top();
-				st.pop();
-				st.push(num1 + num2);
-			}
-			else if (c == '-') {
-				int num1 = st.top();
-				st.pop();
-				int num2 = st.top();
-				st.pop();
-				st.push(num2 - num1);
-			}
-			else if (c == '*') {
-				int num1 = st.top();
-				st.pop();
-				int num2 = st.top();
-				st.pop();
-				st.push(num1 * num2);
-			}
-			else {
-				st.push(c - '0');
-			}
-		}
-		cout << st.top() << "\n";
-		st.pop();
-	}
+void init() {
+    while (!st.empty()) {
+        st.pop();
+    }
 }
 
-/*
-9
-35+
-34*
-23+
-23*5+
-452-+
-36+67*-
-378+-2*
-357+2*+
-3548--+
-*/
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> t;
+    while (t--) {
+        init();
+
+        cin >> str;
+        for (char c: str) {
+            if (c - '0' >= 1 && c - '0' <= 9) {
+                st.push(c - '0');
+            } else {
+                int num2 = st.top();
+                st.pop();
+                int num1 = st.top();
+                st.pop();
+
+                if (c == '+') {
+                    st.push(num1 + num2);
+                } else if (c == '-') {
+                    st.push(num1 - num2);
+                } else if (c == '*') {
+                    st.push(num1 * num2);
+                }
+            }
+        }
+
+        cout << st.top() << "\n";
+        st.pop();
+    }
+}

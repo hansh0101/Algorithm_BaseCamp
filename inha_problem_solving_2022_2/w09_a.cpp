5ab925886ce7307d3ea3635078ba9fea9bee0868
hansh0101 <hansh0101@inha.edu>
2022-12-08 17:31:10 +0900

    solved !

--- inha_problem_solving_2022_2/w09_a.cpp
+++ inha_problem_solving_2022_2/w09_a.cpp
@@ -5,22 +5,22 @@
 
 using namespace std;
 
-int t, n, m, k;
-int start;
-vector<int> heights;
+int t, n, m, k, h, u, v;
+int minHeight, startIndex;
 vector<int> graph[1000];
-vector<int> towns;
 queue<pair<int, int>> q;
-bool visit[1000];
-int result[1000];
+int cost[1000];
 
-void init();
-
-void input();
-
-void solve();
-
-void print();
+void init() {
+    minHeight = 1e9 + 1, startIndex = -1;
+    for (int i = 0; i < 1000; i++) {
+        graph[i].clear();
+        cost[i] = -1;
+    }
+    while (!q.empty()) {
+        q.pop();
+    }
+}
 
 int main() {
     cin.tie(NULL);
@@ -29,110 +29,43 @@ int main() {
     cin >> t;
     while (t--) {
         init();
-        input();
-        solve();
-        print();
-    }
 
-    return 0;
-}
-
-void init() {
-    heights.clear();
-    for (auto & i : graph) {
-        i.clear();
-    }
-    towns.clear();
-    memset(result, -1, sizeof(result));
-    memset(visit, false, sizeof(visit));
-}
-
-void input() {
-    cin >> n >> m >> k;
-
-    int index = -1;
-    long long minHeight = 1e9 + 1;
-    for (int i = 0; i < n; i++) {
-        long long height;
-        cin >> height;
-        if (height < minHeight) {
-            minHeight = height;
-            index = i;
-        }
-    }
-    start = index;
-
-    for (int i = 0; i < m; i++) {
-        int from, to;
-        cin >> from >> to;
-        graph[from].push_back(to);
-        graph[to].push_back(from);
-    }
-
-    for (int i = 0; i < k; i++) {
-        int town;
-        cin >> town;
-        towns.push_back(town);
-    }
-}
-
-void solve() {
-    q.push({start, 0});
-    visit[start] = true;
-    result[start] = 0;
-
-    while (!q.empty())  {
-        int cur = q.front().first;
-        int curCost = q.front().second;
-        q.pop();
-
-        for (int next: graph[cur]) {
-            if (!visit[next]) {
-                q.push({next, curCost + 1});
-                visit[next] = true;
-                result[next] = curCost + 1;
+        cin >> n >> m >> k;
+        for (int i = 0; i < n; i++) {
+            cin >> h;
+            if (minHeight > h) {
+                minHeight = h;
+                startIndex = i;
             }
         }
+
+        for (int i = 0; i < m; i++) {
+            cin >> u >> v;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        cost[startIndex] = 0;
+        q.push({startIndex, cost[startIndex]});
+        while (!q.empty()) {
+            int curIndex = q.front().first;
+            int curCost = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < graph[curIndex].size(); i++) {
+                int nextIndex = graph[curIndex][i];
+                int nextCost = curCost + 1;
+
+                if (cost[nextIndex] == -1) {
+                    cost[nextIndex] = nextCost;
+                    q.push({nextIndex, nextCost});
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            cin >> u;
+            cout << cost[u] << "\n";
+        }
     }
 }
-
-void print() {
-    for (int town: towns) {
-        cout << result[town] << "\n";
-    }
-}
-
-/*
-3
-5 6 4
-10 20 30 40 500001
-0 4
-1 4
-2 0
-2 1
-4 2
-2 3
-4
-3
-0
-15
-6 7 3
-52 125812 32 1 36 9999999
-0 1
-0 3
-0 5
-1 3
-1 5
-2 4
-3 5
-4
-1
-99
-4 3 2
-65 9 15 26
-0 2
-2 3
-1 2
-0
-98
- */

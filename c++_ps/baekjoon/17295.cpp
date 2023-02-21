b617b3da7e05cd85e4811681b910cb9803f7c830
hansh0101 <hansh0101@inha.edu>
2023-02-21 12:57:32 +0900

    solved !

---
+++ c++_ps/baekjoon/17295.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string input;
+    getline(cin, input);
+
+    cout << "Avengers: Endgame\n";
+    return 0;
+}

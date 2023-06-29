ecc8c72a7a9ed87426a79902cd92eede43c7948a
hansh0101 <hansh0101@naver.com>
2023-06-29 14:33:52 +0900

    solved !

--- kotlin_ps/src/main/kotlin/programmers/programmers_161989/161989.kt
+++ kotlin_ps/src/main/kotlin/programmers/programmers_161989/161989.kt
@@ -3,6 +3,16 @@ package programmers.programmers_161989
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
         var answer: Int = 0
+
+        var coloredPosition = 0
+
+        section.forEach { currentPosition ->
+            if (coloredPosition < currentPosition) {
+                coloredPosition = currentPosition + m - 1
+                answer++
+            }
+        }
+
         return answer
     }
 }

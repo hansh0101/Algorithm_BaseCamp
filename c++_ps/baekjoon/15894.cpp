6518f278c5235ccf7287a6d581615d339d1bf0d1
hansh0101 <hansh0101@naver.com>
2023-08-02 03:24:53 +0900

    solved !

---
+++ c++_ps/baekjoon/15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    long long n;
+    cin >> n;
+    cout << 4 * n << "\n";
+
+    return 0;
+}

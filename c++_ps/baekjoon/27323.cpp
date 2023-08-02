9116838ce03a8228ff455a7cc65e8bfb0fc7fd61
hansh0101 <hansh0101@naver.com>
2023-08-03 01:22:32 +0900

    solved !

---
+++ c++_ps/baekjoon/27323.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a * b << "\n";
+
+    return 0;
+}

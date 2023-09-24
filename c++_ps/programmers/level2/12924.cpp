5b23e56e99515299877967b55ca69834939fc0e8
hansh0101 <hansh0101@naver.com>
2023-09-25 00:25:45 +0900

    solved !

---
+++ c++_ps/programmers/level2/12924.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    
+    for (int i = 1; i < n; i++) {
+        int sum = i;
+        for (int j = i + 1; j < n; j++) {
+            sum += j;
+            
+            if (sum == n) {
+                answer++;
+                break;
+            } else if (sum > n) {
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}

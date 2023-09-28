3def5f326cab21c120dd550eb00443cdf61ba42a
hansh0101 <hansh0101@naver.com>
2023-09-29 03:32:44 +0900

    solved !

---
+++ c++_ps/programmers/level2/12980.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    while (n != 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n--;
+            answer++;
+        }
+    }
+
+    return answer;
+}

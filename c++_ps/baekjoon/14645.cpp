a0c3ab4e0c1256458c92d6a7202bd69996d15d54
hansh0101 <hansh0101@naver.com>
2023-11-04 08:12:43 +0900

    solved !

---
+++ c++_ps/baekjoon/14645.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    int a, b;
+
+    cin >> n >> k;
+    for (int i = 0; i < 4; i++) {
+        cin >> a >> b;
+    }
+
+    cout << "비와이\n";
+    return 0;
+}

ea35ceb4d7293a7c6a828ba0fbd2abf3d4cf0282
hansh0101 <hansh0101@naver.com>
2023-11-07 15:51:13 +0900

    solved !

---
+++ c++_ps/baekjoon/15680.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << "YONSEI\n";
+    } else if (n == 1) {
+        cout << "Leading the Way to the Future\n";
+    }
+
+    return 0;
+}
